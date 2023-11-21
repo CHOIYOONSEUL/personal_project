bcfbe59d0e25f1f152b342f7b4d62a6884b1dc36
yoonseul <choieb9519@gmail.com>
2023-11-21 10:21:09 +0900

    hi

---
+++ src/main/java/variableExample.java
@@ -0,0 +1,9 @@
+public class variableExample {
+    public static void main(String[] args) {
+
+        int value = 10;
+        int result = value + 30;
+
+        System.out.println(result);
+    }
+}
